4ea0e84aa0afb312ce28df381407b009ec5bf395
Promlin <somebody2605@yandex.ru>
2020-10-21 14:19:21 +0300

    Создание шаблона для одиночки и двух потоков в Program

---
+++ hw4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainPrintMethod();
+
+            Console.WriteLine("Finish of 1 method's work");
+
+            //Создание потока и вызов метода из класса Program
+            ThreadStart currentTask = new ThreadStart(MainPrintMethod);
+            var thread = new Thread(currentTask);
+            thread.Start();
+
+        }
+
+        static void MainPrintMethod()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write("Main i " + i);
+            }
+        }
+    }
+}
---
+++ hw4/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public static void printString()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("Singleton i " + i);
+            }
+        }
+    }
+}
