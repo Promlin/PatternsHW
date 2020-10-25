3f382a7774a526f667cac85fee5c297fa7ad7158
Promlin <somebody2605@yandex.ru>
2020-10-25 11:00:25 +0300

    Заместитель

---
+++ hw5/CalculatingClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+
+namespace hw5
+{
+    class CalculatingClass : ICalculating
+    {
+
+        public float Dividing(float a, float b) //деление
+        {
+            return a / b;
+        }
+
+        public float Multiply(float a, float b)  //умножение
+        {
+            return a * b;
+        }
+
+        public float Substraction(float a, float b) //вычитание
+        {
+            return a - b;
+        }
+
+        public float Sum(float a, float b)  //сложение
+        {
+            return a + b;
+        }
+    }
+}
---
+++ hw5/ICalculating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw5
+{
+    interface ICalculating
+    {
+        float Sum(float a, float b);
+        float Multiply(float a, float b);
+        float Substraction(float a, float b);
+        float Dividing(float a, float b);
+
+    }
+}
