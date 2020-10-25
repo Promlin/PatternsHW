5bd1309accc70171b814477b5d1dddbd2294127f
Promlin <somebody2605@yandex.ru>
2020-10-25 11:20:53 +0300

    Приспособленец

---
+++ hw5/task1/FigureFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace hw5.task1
+{
+    public class FigureFactory 
+    {
+        static Dictionary<string, IDrawFigure> figures = new Dictionary<string, IDrawFigure>();
+
+        public static IDrawFigure GetFigures(string figureType)
+        {
+            switch (figureType)
+            {
+                case "Окружность":
+                    if (!figures.ContainsKey("Окружность"))
+                        figures["Окружность"] = new DrawCircle();
+                    return figures["Окружность"];
+                case "Квадрат":
+                    if (!figures.ContainsKey("Квадрат"))
+                        figures["Квадрат"] = new DrawSquare();
+                    return figures["Квадрат"];
+                default: break;
+            }
+            return null;
+        }
+    }
+}
