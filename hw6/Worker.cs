336282f7f5904c9db386e2da73169c2fe355a346
Promlin <somebody2605@yandex.ru>
2020-10-28 16:19:01 +0300

    цепочка обязанностей

---
+++ hw6/Worker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw6
+{
+    public abstract class Worker
+    {
+        protected WorkerLevel level;
+        protected Worker nextWorker;
+        public void SetNextWorker(Worker nextWorker) => this.nextWorker = nextWorker;
+        public void WorkerInfo(WorkerLevel level, string message)
+        {
+            if (this.level <= level) Write(message);
+            if (nextWorker != null) nextWorker.WorkerInfo(level, message);
+        }
+
+        abstract protected void Write(string message);
+    }
+}
---
+++ hw6/WorkerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw6
+{
+    public enum WorkerLevel
+    {
+        UsualWorker = 1,
+        WorkersBoss,
+        Director
+    }
+}
