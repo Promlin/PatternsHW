59b16c49b0bd38d6aa0a8f5d06088f16446e4f40
Promlin <somebody2605@yandex.ru>
2020-11-01 03:17:29 +0300

    реализация паттерна "Стратегия" с использованием делегатов

---
+++ hw7/ShippingCostCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw7
+{
+    public class ShippingCostCalculator
+    {
+        public double Calculate(Order order, Func<Order, double> shippingCostStrategy)
+        {
+            return shippingCostStrategy(order);
+        }
+    }
+}
